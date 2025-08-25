abbd2c19b33182a417981ec4862c4710fd7a88bc
Juan Sebastián Montaño Pérez <sp6027193@gmail.com>
2025-08-25 15:17:17 -0600

    das

---
+++ CentralizedApps/Controllers/web/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CentralizedApps.Controllers.web
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+
+    }
+}
--- CentralizedApps/Program.cs
+++ CentralizedApps/Program.cs
@@ -9,62 +9,48 @@ using CentralizedApps.Services;
 using CentralizedApps.Services.Interfaces;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 using System.Net;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Puerto y HTTPS
+// Configuración de Kestrel
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5107); // HTTP
-    options.ListenAnyIP(7081, listenOptions =>
-    {
-        listenOptions.UseHttps(); // HTTPS (certificado necesario)
-    });
+    options.ListenAnyIP(5107);
+    options.ListenAnyIP(7081, listenOptions => listenOptions.UseHttps());
 });
 
-// Fluent Validation
+
+
+// FluentValidation
 builder.Services
     .AddFluentValidationAutoValidation()
     .AddFluentValidationClientsideAdapters();
 builder.Services.AddValidatorsFromAssemblyContaining<CustomerValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<LoginValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<ProcedureValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<DocumentTypeValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<QueryFielValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<AvailibityValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<MunicipalityValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateCourseValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateSportsFacilityvalidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<DepartmentValidator>();
 
+// MVC y API
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddControllers();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "CentralizedApps API",
-        Version = "v1",
-        Description = "Documentación de la API Centralizada"
-    });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CentralizedApps API", Version = "v1" });
 });
+
+// AutoMapper
 builder.Services.AddAutoMapper(typeof(MunicipalityProfile));
 
-ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 // Base de datos
 builder.Services.AddDbContext<CentralizedAppsDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("ConectionDefault"),
         sqlOptions => sqlOptions.CommandTimeout(180)
-    )
-);
+    ));
 
 // Repositorios y servicios
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
@@ -77,9 +63,32 @@ builder.Services.AddScoped<IPaymentHistoryService, PaymentHistoryService>();
 builder.Services.AddScoped<IProcedureServices, ProcedureServices>();
 builder.Services.AddScoped<IBank, BankService>();
 
-var app = builder.Build();
+// Claims / Autenticación
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
-// Middleware personalizado de errores
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireClaim("Role", "Admin"));
+});
+
+var app = builder.Build();
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/Account/Login");
+    return Task.CompletedTask;
+});
+
+// Middleware
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
 // Swagger
@@ -89,14 +98,10 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "CentralizedApps API v1");
     c.RoutePrefix = "swagger";
 });
-app.UseStaticFiles();
-app.UseAuthorization();
 
-// WEB (Razor)
+// Rutas MVC
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
 app.Run();
-
