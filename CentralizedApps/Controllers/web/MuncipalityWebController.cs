d0bf77c7450c839c38f535161a8897ab943b8e45
desarollofasemovil <desarrollofasemovil@hotmial.com>
2025-09-24 11:32:28 -0500

    municipios

--- CentralizedApps/Controllers/web/MuncipalityWebController.cs
+++ CentralizedApps/Controllers/web/MuncipalityWebController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CentralizedApps.Models.Dtos;
+using CentralizedApps.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,10 +12,37 @@ namespace CentralizedApps.Controllers.web
 {
     public class MuncipalityWebController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly IMunicipalityServices _MunicipalityServices;
+        public MuncipalityWebController(IMunicipalityServices MunicipalityServices)
         {
-            return View();
+            _MunicipalityServices = MunicipalityServices;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var response = await _MunicipalityServices.GetAllMunicipalityWithRelations();
+                if (response == null || !response.Any())
+                {
+                    return View(new ValidationResponseDto
+                    {
+                        BooleanStatus = false,
+                        CodeStatus = 404,
+                        SentencesError = "No se encontraron municipios con relaciones."
+                    });
+                }
+                
+                return View(response);
+            }
+            catch (Exception ex)
+            {
+                return View(new ValidationResponseDto
+                {
+                    BooleanStatus = false,
+                    CodeStatus = 500,
+                    SentencesError = $"Error extraño ${ex.Message}"
+                });
+            }
         }
 
     }
